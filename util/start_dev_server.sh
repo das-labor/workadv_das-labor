d1c7d973ac46183c38aa0b66368fbf656fcd6665
Marco Bakera <marco@bakera.de>
2021-01-02 13:42:11 +0100

    adding logfile for server process.

--- util/start_dev_server.sh
+++ util/start_dev_server.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
 PORT=8003
+LOGFILE=$0.log
 
 # starting http server
 cd $(dirname $0)/.. 
-python3 util/no_cors_server.py $PORT &
+python3 util/no_cors_server.py $PORT 2>&1 >> $LOGFILE &
+
 # using ngrok to relay
 ngrok http $PORT
 
