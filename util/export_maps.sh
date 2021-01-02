b809384ded9d746345fabdf3242bb6b353c7b8d3
Marco Bakera <marco@bakera.de>
2021-01-02 10:35:48 +0100

    added util for map export.

---
+++ util/export_maps.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# exporting tiled maps to image files
+
+cd $(dirname $0)/..
+for f in *.json ; do
+	f_base=$(basename $f .json)
+	echo "converting $f to png"
+	tiled --export-map tmx $f $f_base.tmx
+	tmxrasterizer $f_base.tmx $f_base.png
+	rm $f_base.tmx
+done
+
