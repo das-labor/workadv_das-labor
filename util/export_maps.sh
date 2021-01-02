3601765fa99fe4434d1501d13ed73cf2b6a760f9
pintman <pintman@users.noreply.github.com>
2021-01-02 18:20:41 +0000

    Deploying to gh-pages from master @ 0b21cad2b5c57cb15673f5e9336731884508b887 🚀

--- util/export_maps.sh
+++ util/export_maps.sh
@@ -2,12 +2,12 @@
 
 # exporting tiled maps to image files
 
-cd $(dirname $0)/..
+cd "$(dirname "$0")"/.. || exit
 for f in *.json ; do
-	f_base=$(basename $f .json)
+	f_base=$(basename "$f" .json)
 	echo "converting $f to png"
-	tiled --export-map tmx $f $f_base.tmx
-	tmxrasterizer $f_base.tmx $f_base.png
-	rm $f_base.tmx
+	tiled --export-map tmx "$f" "$f_base.tmx"
+	tmxrasterizer "$f_base.tmx" "$f_base.png"
+	rm "$f_base.tmx"
 done
 
