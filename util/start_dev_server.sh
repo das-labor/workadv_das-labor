8b53c02b91849a21f400851be82c8ade3a9750d9
Pintman <pintman@0xabc.de>
2021-01-02 22:34:17 +0100

    killing server if ngrok completes.

--- util/start_dev_server.sh
+++ util/start_dev_server.sh
@@ -6,7 +6,10 @@ LOGFILE=$0.log
 # starting http server
 cd "$(dirname "$0")/.." || exit
 python3 util/no_cors_server.py "$PORT" >> "$LOGFILE" 2>&1 &
+P1="$!"
 
 # using ngrok to relay
 ngrok http "$PORT"
+
+kill "$P1"
 
