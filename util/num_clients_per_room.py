9f364773aa1896439fa950cfd59277edbe7a7e74
Marco Bakera <marco@bakera.de>
2021-03-14 21:48:07 +0100

    feat: listing all rooms an num clients

--- util/num_clients_per_room.py
+++ util/num_clients_per_room.py
@@ -1,18 +1,22 @@
 from urllib.request import urlopen
 
-pusher_url = "https://pusher.wa.binary-kitchen.de"
-room = "_/global/das-labor.github.io/workadv_das-labor/main.json"
+PUSHER_URL = "https://pusher.wa.binary-kitchen.de"
+ROOM_PREFIX = "_/global/das-labor.github.io/workadv_das-labor"
+METRICS_URL = PUSHER_URL + "/metrics"
+NUM_CLIENTS_MARKER = 'workadventure_nb_clients_per_room'
 
-metrics_url = pusher_url + "/metrics"
-
-with urlopen(metrics_url) as f:
+with urlopen(METRICS_URL) as f:
     lines = f.readlines()
 
+print('Number of clients per room in', ROOM_PREFIX)
 for line in lines:
     line_str = line.decode()
-    if room in line_str:
+    if NUM_CLIENTS_MARKER in line_str and \
+        ROOM_PREFIX in line_str:
+
         # line of format
         # workadventure_nb_clients_per_room{room="_/global/das-labor.github.io/workadv_das-labor/main.json"} -280
         num = int(line_str.split(' ')[1])
+        room = line_str.split(ROOM_PREFIX)[1].split('.json')[0]
 
-print(num, "clients online")
+        print(num, "in", room)
